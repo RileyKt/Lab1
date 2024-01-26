ac63f84c657ff6beb66c69f93fc0efe1435b2235
Riley Kneen-Teed <kneenrt@gmail.com>
2024-01-25 21:45:29 -0400

    Student Manager, Ready for marking

---
+++ Module 1/Section3/PassCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LoopCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var j =0; j<5; j++ )
+            {
+                for(var i =1; i<10;i++)
+                {
+                       Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Module 1/Section4/PassCode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StudentManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many students are in your class?");
+            var studentCount = int.Parse(Console.ReadLine());
+
+            var studentInfo = new string[studentCount, 2];
+
+            for (int i= 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Enter the name of student "+(i+1)+":");
+                studentInfo[i,0] = Console.ReadLine();
+
+                Console.WriteLine("Student Grade: ");
+                studentInfo[i, 1] = Console.ReadLine();
+            }
+
+            for (int i=0; i<studentCount; i++)
+            {
+                Console.WriteLine("Name: " + studentInfo[i,0] + ", Grade: " + studentInfo[i,1]);
+            }
+        }
+    }
+}
