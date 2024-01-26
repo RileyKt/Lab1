3c080a9e1caaa5aed81fa3d0b1b84cd0de7ada89
Riley Kneen-Teed <kneenrt@gmail.com>
2024-01-25 20:05:03 -0400

    finished passcode

---
+++ Module 1/Section1/Module1/Module1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Survey{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Welcome to the survey");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born in?");
+            string month = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite food?");
+            string food = Console.ReadLine();
+            
+            Console.WriteLine("What is your favorite sport?");
+            string sport = Console.ReadLine();
+
+
+
+            Console.WriteLine("Your name is " + name);
+            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("Your birth month is " + month);
+            Console.WriteLine("Your favorite food is " + food);
+            Console.WriteLine("Your favorite sport is " + sport);
+            Console.WriteLine("Press any key to exit");
+
+        }
+    }
+}
---
+++ Module 1/Section2/PassCode/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PassCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secret = "secret";
+            var exit = 0;
+            while(exit == 0){ 
+                Console.WriteLine("What is the pass code?");
+                var code = Console.ReadLine();
+
+                if(code == secret)
+                {
+                    Console.WriteLine("Authenticated");
+                    Console.WriteLine("What is the new pass code?");
+                    secret = Console.ReadLine();
+                    Console.WriteLine("Would you like to continue? (y/n)");
+                    var answer = Console.ReadLine();
+                    if(answer == "n")
+                    {
+                        exit = 1;
+                    }
+                    else
+                    {
+                        exit = 0;
+                    }
+                    
+                                        
+
+                }
+                else
+                {
+                    Console.WriteLine("Not Authenticated");
+                }
+            }
+        }
+    }
+}
